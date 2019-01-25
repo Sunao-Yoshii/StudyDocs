85b298db9af19f36c379869f0c16f091d1e5713f
Sunao Yoshii <2810066+Sunao-Yoshii@users.noreply.github.com>
2019-01-25 22:37:00 +0900

    I/O を機能として外部切り出しし、数字入力をメソッド化。

--- ase_class_room/RefactorExample/src/main/java/net/white/azalea/VendingMachine.java
+++ ase_class_room/RefactorExample/src/main/java/net/white/azalea/VendingMachine.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VendingMachine {
@@ -17,18 +18,17 @@ public class VendingMachine {
                     new Product(120, "練乳珈琲"),
                     new Product(90, "へ〜いお茶")
             );
+            IOUtils util = new IOUtils(reader, System.out);
 
-            new VendingMachine(reader, System.out, products).buy();
+            new VendingMachine(util, products).buy();
         }
     }
 
-    private final BufferedReader reader;
-    private final PrintStream out;
     private final List<Product> products;
+    private final IOUtils ioUtils;
 
-    public VendingMachine(BufferedReader reader, PrintStream printer, List<Product> products) {
-        this.reader = reader;
-        this.out = printer;
+    public VendingMachine(IOUtils ioUtils, List<Product> products) {
+        this.ioUtils = ioUtils;
         this.products = products;
     }
 
@@ -38,17 +38,16 @@ public class VendingMachine {
      */
     public void buy() {
         try {
+            // ジュースリスト表示
+            this.ioUtils.println("ジュースの選択");
+            this.ioUtils.println(this.getProductsString());
+
             // ジュース選択
-            out.println("ジュースの選択");
-            for (int i = 0; i < products.size(); i++) {
-                Product p = products.get(i);
-                out.println(String.format("%d: Item: %s (%d)", i, p.getName(), p.getPrice()));
-            }
             int n = 999;
             while (n >= products.size()) {
-                n = Integer.parseInt(reader.readLine());
+                n = this.ioUtils.readInteger();
                 if (n >= products.size()) {
-                    out.println("選択出来るジュースを選んでください");
+                    this.ioUtils.println("選択出来るジュースを選んでください");
                 }
             }
 
@@ -56,43 +55,52 @@ public class VendingMachine {
             Product selected = products.get(n);
 
             // コインの挿入
-            out.println("コインを入れてね(使用可能: 500, 100, 50, 10)");
+            this.ioUtils.println("コインを入れてね(使用可能: 500, 100, 50, 10)");
             while (n < selected.getPrice()) {
-                int coin = Integer.parseInt(reader.readLine());
+                int coin = this.ioUtils.readInteger();
                 if (List.of(500, 100, 50, 10).contains(coin)) {
                     n += coin;
                 } else {
-                    out.println("その硬貨は使えません");
+                    this.ioUtils.println("その硬貨は使えません");
                 }
             }
 
             // 釣り計算
             int r = n - selected.getPrice();
-            out.println("お釣り硬貨は以下の通りです。");
+            this.ioUtils.println("お釣り硬貨は以下の通りです。");
             while (r > 0) {
                 if (r > 500) {
-                    out.println("500円");
+                    this.ioUtils.println("500円");
                     r -= 500;
                     continue;
                 }
                 if (r > 100) {
-                    out.println("500円");
+                    this.ioUtils.println("100円");
                     r -= 500;
                     continue;
                 }
                 if (r > 50) {
-                    out.println("500円");
+                    this.ioUtils.println("50円");
                     r -= 500;
                     continue;
                 }
                 if (r > 10) {
-                    out.println("500円");
+                    this.ioUtils.println("10円");
                     r -= 500;
                 }
             }
         } catch (Exception e) {
-            out.println(e.toString());
+            this.ioUtils.println(e.toString());
         }
+    }
+
+    public String getProductsString() {
+        List<String> productStrings = new ArrayList<>();
+        for (int i = 0; i < products.size(); i++) {
+            Product p = products.get(i);
+            productStrings.add(String.format("%d: Item: %s (%d)", i, p.getName(), p.getPrice()));
+        }
+        return String.join("\n", productStrings);
     }
 }
 
@@ -114,3 +122,27 @@ class Product {
     }
 }
 
+
+class IOUtils {
+    private final BufferedReader reader;
+    private final PrintStream out;
+
+    public IOUtils(BufferedReader reader, PrintStream out) {
+        this.reader = reader;
+        this.out = out;
+    }
+
+    public void println(String v) {
+        this.out.println(v);
+    }
+
+    public int readInteger() throws IOException {
+        while(true) {
+            try {
+                return Integer.parseInt(reader.readLine());
+            } catch (NumberFormatException e) {
+                this.out.println("整数を入力してください。");
+            }
+        }
+    }
+}
