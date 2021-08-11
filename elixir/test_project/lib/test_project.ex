4149df355f916621ce8e2989b8be720a0faabb62
Sunao-Yoshii <2810066+Sunao-Yoshii@users.noreply.github.com>
2021-08-11 21:51:09 +0900

    各種勉強後をコミット

---
+++ elixir/test_project/lib/test_project.ex
@@ -0,0 +1,22 @@
+defmodule TestProject do
+  @moduledoc """
+  Documentation for TestProject.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestProject.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def add(a, b) do
+    a + b
+  end
+end
